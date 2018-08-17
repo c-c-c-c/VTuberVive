70e7ffc7901632fa95a201605536812c3d4e5bc6
c-c-c-c <4c.mura@gmail.com>
2018-08-17 18:11:11 +0900

    singletonを追加

---
+++ Assets/MyScript/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager StaticGameManager
+    {
+        get; private set;
+    }
+    public int score = 0;
+
+    void Awake()
+    {
+        if (StaticGameManager != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        StaticGameManager = this;
+        DontDestroyOnLoad(gameObject);
+
+        score = 30;
+    }
+
+    // click callback
+    void Update()
+    {
+        //Application.LoadLevel(1);
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            score += 30;
+            print(score);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("DanceStudioScene");
+        }
+
+    }
+
+}
---
+++ Assets/MyScript/RotateRadarCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateRadarCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform character;
+
+    void Update()
+    {
+        var rot = Quaternion.Slerp(transform.rotation, character.rotation, Time.deltaTime);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, rot.eulerAngles.y, transform.eulerAngles.z);
+    }
+}
